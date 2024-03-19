7184cd1d9b2b9c8642a0ad549d1ea49057c2da21
suhuamo <1185922537@qq.com>
2024-03-19 23:25:29 +0800

    delete： 1. 删除无用文件和注释

--- src/main/java/com/suhuamo/init/ApplicationMain.java
+++ src/main/java/com/suhuamo/init/ApplicationMain.java
@@ -9,7 +9,6 @@ import java.io.IOException;
 @SpringBootApplication
 public class ApplicationMain {
     public static void main(String[] args) throws IOException {
-        // TODO: 2024/3/10 生成的DTO字段修改，Query字段的增加，mysql连接地址的时区配置，生成实体类时不能覆盖已经有的文件
         SpringApplication.run(ApplicationMain.class, args);
     }
 }
