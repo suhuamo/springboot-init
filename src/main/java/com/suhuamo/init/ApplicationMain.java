57182c753472bf503be46d8dcef0237bcb03f913
suhuamo <1185922537@qq.com>
2024-03-27 22:38:31 +0800

    add: 1. 重新添加todo，并添加了排除sql自动注入的配置类

--- src/main/java/com/suhuamo/init/ApplicationMain.java
+++ src/main/java/com/suhuamo/init/ApplicationMain.java
@@ -1,15 +1,31 @@
 package com.suhuamo.init;
 
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 import java.io.IOException;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class ,
+        DruidDataSourceAutoConfigure.class})
 public class ApplicationMain {
     public static void main(String[] args) throws IOException {
-        // TODO: 2024/3/20 添加分页查询xml语句,整理自动生成代码的util【生成vo和dto的通用提取】，重新设计CodeEnum和全局异常，DTO加上@NotNUll和接口加上@Valid;使用注解过滤当前用户权限数据；ThreadLocalUtil添加getUser方法 [by:yuanchuncheng]
+        // TODO: 2024/3/20 如下 [by:yuanchuncheng]
+        /*
+         1. 添加分页查询xml语句
+         2. 整理自动生成代码的util【生成vo和dto的通用提取】
+         3. 重新设计CodeEnum和全局异常
+         4. DTO加上@NotNUll和接口加上@Valid
+         5. 使用注解过滤当前用户权限数据
+         6. ThreadLocalUtil添加getUser方法
+         */
         SpringApplication.run(ApplicationMain.class, args);
     }
 }
