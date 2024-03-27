3089026ecdf238a08a3fc1df98acbf5dca29e15f
suhuamo <1185922537@qq.com>
2024-03-27 22:37:46 +0800

    add: 1. 重新整理jwt的放行接口路径

--- src/main/java/com/suhuamo/init/config/JwtWebConfig.java
+++ src/main/java/com/suhuamo/init/config/JwtWebConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author suhuamo
  * @date 2023-12-31
@@ -15,6 +19,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class JwtWebConfig implements WebMvcConfigurer {
 
     /**
+     * swagger文档相关路径
+     */
+    private final String[] swaggerUrl = new String[]{"/doc*/**","/swagger-resources/**", "/webjars/**", "/v2/**", "/swagger-ui.html/**",
+            "/api", "/api-docs", "/api-docs/**", "/doc.html/**" };
+
+    /**
+     * 用户登录、注册路径
+     */
+    private final String[] userUrl = new String[]{"/users/login", "/users/register"};
+
+    /**
      * 添加拦截器
      *
      * @param registry
@@ -22,11 +37,15 @@ public class JwtWebConfig implements WebMvcConfigurer {
      */
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        List<String> excludePathPatternList = new ArrayList<>();
+        excludePathPatternList.add("/index.html"); //普通页面--后端首页
+        excludePathPatternList.add("/upload/index.html"); //普通页面--测试上传文件页面
+        excludePathPatternList.add("/redirect/*"); // 路径跳转接口
+        excludePathPatternList.addAll(Arrays.asList(userUrl));
+        excludePathPatternList.addAll(Arrays.asList(swaggerUrl));
         registry.addInterceptor(new JwtInterceptor()) // 添加自定义拦截器
                 .addPathPatterns("/**")// 设置映射规则,即允许哪些接口会被添加到拦截器中
-                .excludePathPatterns("/users/login", "/users/add" // 用户登录接口
-                        , "/doc*/**","/swagger-resources/**", "/webjars/**", "/v2/**", "/swagger-ui.html/**", // swagger接口
-                        "/api", "/api-docs", "/api-docs/**", "/doc.html/**" // swagger接口
-                        , "*.html", "/redirect/*"); //设置排除的规则，即哪些接口不会被加入到拦截器中
+                .excludePathPatterns(excludePathPatternList); //设置排除的规则，即哪些接口不会被加入到拦截器中
+
     }
 }
